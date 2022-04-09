ad914c51b9c72a8c0b13917ae9105c0e16359f63
AwosKanaan <79727246+AwosKanaan@users.noreply.github.com>
2022-04-09 15:08:34 +0300

    Add files via upload
    ArraySort

--- Main.java
+++ Main.java
@@ -1,25 +1,50 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main
 {
-    public static void main(String[] args)
-    {
-        printFactors(10);
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int[] myArray = getIntegers(5);
+        printArray(sortIntegers(myArray));
+
     }
 
-    public static void printFactors(int number)
-    {
-        if (number < 1)
-        {
-            System.out.println("Invalid Value");
+    public static int[] getIntegers(int number) {
+        int[] array = new int[number];
+
+        System.out.println("Enter " + array.length + " Elements:");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = input.nextInt();
+        }
+        return array;
+    }
+
+    public static void printArray(int[] arrayName) {
+        for (int i = 0; i < arrayName.length; i++) {
+            System.out.println(arrayName[i]);
+        }
+    }
+
+    public static int[] sortIntegers(int[] arr) {
+        int temp = 0;
+        int[] sortedArray = new int[arr.length];
+        for (int a = 0; a < arr.length; a++) {
+            sortedArray[a] = arr[a];
         }
 
-        for (int i = 1; i <= number; i++)
-        {
-            if ((number % i) == 0)
-            {
-                System.out.println(i);
+        for (int i = 0; i < sortedArray.length; i++) {
+            for (int j = i + 1; j < sortedArray.length; j++) {
+                if (sortedArray[i] < sortedArray[j]) {
+                    temp = sortedArray[i];
+                    sortedArray[i] = sortedArray[j];
+                    sortedArray[j] = temp;
+                }
             }
         }
+        return sortedArray;
     }
 }
