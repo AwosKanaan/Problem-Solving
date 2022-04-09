ce2477d88f3318222dc37f71a234f3056cab10ad
AwosKanaan <79727246+AwosKanaan@users.noreply.github.com>
2022-04-09 15:19:30 +0300

    Add files via upload
    AreEqualByThreeDecimalPlaces

--- Main.java
+++ Main.java
@@ -1,50 +1,29 @@
 package com.company;
 
-import java.util.Scanner;
-
 public class Main
 {
-    private static Scanner input = new Scanner(System.in);
+    public static void main(String[] args)
+    {
 
-    public static void main(String[] args) {
+	}
+    public static boolean areEqualByThreeDecimalPlaces(double first,double second) {
 
-        int[] myArray = getIntegers(5);
-        printArray(sortIntegers(myArray));
+        if (first <0 && second >0 || first >0 && second <0){
+            return false;
+        }
+
+        double firstThousand = first*1000;
+        double secondThousand = second*1000;
+
+        int firstint = (int)firstThousand;
+        int secondint = (int)secondThousand;
+
+        if (firstint==secondint){
+            return true;
+        }
+
+        return false;
 
     }
 
-    public static int[] getIntegers(int number) {
-        int[] array = new int[number];
-
-        System.out.println("Enter " + array.length + " Elements:");
-        for (int i = 0; i < array.length; i++) {
-            array[i] = input.nextInt();
-        }
-        return array;
-    }
-
-    public static void printArray(int[] arrayName) {
-        for (int i = 0; i < arrayName.length; i++) {
-            System.out.println(arrayName[i]);
-        }
-    }
-
-    public static int[] sortIntegers(int[] arr) {
-        int temp = 0;
-        int[] sortedArray = new int[arr.length];
-        for (int a = 0; a < arr.length; a++) {
-            sortedArray[a] = arr[a];
-        }
-
-        for (int i = 0; i < sortedArray.length; i++) {
-            for (int j = i + 1; j < sortedArray.length; j++) {
-                if (sortedArray[i] < sortedArray[j]) {
-                    temp = sortedArray[i];
-                    sortedArray[i] = sortedArray[j];
-                    sortedArray[j] = temp;
-                }
-            }
-        }
-        return sortedArray;
-    }
 }
